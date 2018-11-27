d455d5f77aaf33cfba8ff8223034ae16a14ca468
Cayce House <cayce@houseco.cc>
2018-11-26 20:57:33 -0500

    Reverted index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,3 @@
 import React from 'react';
 
-const test = ['hello', 'eyllow', 'sdf'];
-
-export default () => (
-  <div>
-    {test}
-    Hello world!
-  </div>
-);
+export default () => <div>Hello world!</div>;
