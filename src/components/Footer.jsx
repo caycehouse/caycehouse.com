2e595cffb81f9e5371312ace29d811efa1c54c9b
Cayce House <cayce@houseco.cc>
2018-11-27 16:33:07 -0500

    Fixed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,9 +4,8 @@ export default () => (
   <footer className="footer bg-light">
     <div className="container text-center">
       <span className="text-black-50">
-        Copyright &copy;
-        {` 2010 - ${new Date().getFullYear()} `}
-        {'Cayce House | Powered by '}
+        &copy;
+        {` ${new Date().getFullYear()} Cayce House | Powered by `}
         <a href="//sumojoe.com" target="_blank" rel="noopener noreferrer">
           SUMOjoe
         </a>
