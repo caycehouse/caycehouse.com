2b95bf5dab521c2cade805acd6f3ae75538e568c
Cayce House <cayce@houseco.cc>
2018-11-26 23:28:00 -0500

    Moved bootstrap css location

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import Header from './Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Layout = ({ children }) => (
   <div>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default () => (
   <Layout>
