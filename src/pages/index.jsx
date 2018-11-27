154fd89fa0d08f3f8caaf41ff12a9ee484a4d546
Cayce House <cayce@houseco.cc>
2018-11-27 13:49:40 -0500

    Changed bg color

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,43 +5,56 @@ import profileImg from './profile-img.jpeg';
 import Layout from '../components/Layout';
 import SocialIcons from '../components/SocialIcons';
 
-export default () => (
-  <Layout>
-    <React.Fragment>
-      <Particles
-        style={{ position: 'fixed', zIndex: '-1' }}
-        params={{
-          particles: {
-            number: {
-              value: 50,
-            },
-            size: {
-              value: 3,
-            },
-          },
-          interactivity: {
-            events: {
-              onhover: {
-                enable: true,
-                mode: 'repulse',
+export default class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    document.body.style.backgroundColor = '#525ddc';
+  }
+
+  componentWillUnmount() {
+    document.body.style.backgroundColor = null;
+  }
+
+  render() {
+    return (
+      <Layout>
+        <React.Fragment>
+          <Particles
+            style={{ position: 'fixed', zIndex: '-1' }}
+            params={{
+              particles: {
+                number: {
+                  value: 50,
+                },
+                size: {
+                  value: 3,
+                },
               },
-            },
-          },
-        }}
-      />
-      <div className="text-center mt-5">
-        <img
-          alt="Cayce House"
-          className="img-responsive rounded-circle"
-          src={profileImg}
-          style={{ maxHeight: '250px' }}
-        />
-        <h1 className="display-2 mt-2">Cayce House</h1>
-        <h2>Web & App Developer</h2>
-        <div className="mt-5">
-          <SocialIcons />
-        </div>
-      </div>
-    </React.Fragment>
-  </Layout>
-);
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: 'repulse',
+                  },
+                },
+              },
+            }}
+          />
+          <div className="text-center mt-5">
+            <img
+              alt="Cayce House"
+              className="img-responsive rounded-circle"
+              src={profileImg}
+              style={{ maxHeight: '250px' }}
+            />
+            <h1 className="display-2 mt-2">Cayce House</h1>
+            <h2>Web & App Developer</h2>
+            <div className="mt-5">
+              <SocialIcons />
+            </div>
+          </div>
+        </React.Fragment>
+      </Layout>
+    );
+  }
+}
