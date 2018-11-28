847727c19f3ec3dd2c19e112dce946caa5bf581b
Cayce House <cayce@houseco.cc>
2018-11-27 21:10:18 -0500

    Copyright text color change

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default () => (
   <footer className="footer bg-light">
     <div className="container text-center">
-      <span className="text-black-50">
+      <span className="text-muted">
         &copy;
         {` ${new Date().getFullYear()} Cayce House | Powered by `}
         <a href="//sumojoe.com" target="_blank" rel="noopener noreferrer">
