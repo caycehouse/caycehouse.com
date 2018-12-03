0a6cf3be6065d9bf205bbdcd54e1b84a7a6e11f8
Cayce House <cayce@houseco.cc>
2018-12-03 14:54:39 -0500

    Fixed 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
+import Header from '../components/Header';
+
 export default () => (
-  <div className="text-center mt-5">
-    <h1>Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-    <button
-      type="button"
-      className="btn btn-link"
-      onClick={() => window.history.go(-1)}
-    >
-      Go Back
-    </button>
-  </div>
+  <React.Fragment>
+    <Header />
+    <div className="text-center mt-5">
+      <h1>Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <button
+        type="button"
+        className="btn btn-link"
+        onClick={() => window.history.go(-1)}
+      >
+        Go Back
+      </button>
+    </div>
+  </React.Fragment>
 );
