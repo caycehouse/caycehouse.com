0c06092752b03a4a12521f32b2a557e8517bee6d
Cayce House <cayce@houseco.cc>
2018-12-04 17:10:16 -0500

    Cleaned up index

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,62 +2,57 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
+import { createGlobalStyle } from 'styled-components';
 
 import Layout from '../components/Layout';
 import SocialIcons from '../components/SocialIcons';
 
-export default class Index extends React.Component {
-  componentDidMount() {
-    document.body.style.backgroundColor = '#525ddc';
-    document.body.style.color = '#fff';
+const GlobalStyle = createGlobalStyle`
+  body {
+    color: #ffffff;
   }
+`;
 
-  componentWillUnmount() {
-    document.body.style.backgroundColor = null;
-    document.body.style.color = null;
-  }
-
-  render() {
-    const { data } = this.props;
-    return (
-      <Layout>
-        <React.Fragment>
-          <div className="text-center mt-5">
-            <Img
-              alt="Cayce House"
-              fixed={data.profileImg.childImageSharp.fixed}
-              className="img-responsive rounded-circle"
-              style={{
-                zIndex: 999,
-                position: 'relative',
-              }}
-            />
-            <h1 className="display-2 mt-2">Cayce House</h1>
-            <h2>Web & App Developer</h2>
-            <div className="mt-5">
-              <SocialIcons />
-            </div>
-          </div>
-          <Img
-            fluid={data.bgImg.childImageSharp.fluid}
-            style={{
-              position: 'absolute',
-              left: 0,
-              top: 0,
-              width: '100%',
-              height: '100%',
-              zIndex: -1,
-            }}
-          />
-        </React.Fragment>
-      </Layout>
-    );
-  }
-}
+const Index = ({ data }) => (
+  <Layout>
+    <React.Fragment>
+      <div className="text-center mt-5">
+        <Img
+          alt="Cayce House"
+          fixed={data.profileImg.childImageSharp.fixed}
+          className="img-responsive rounded-circle"
+          style={{
+            zIndex: 999,
+            position: 'relative',
+          }}
+        />
+        <h1 className="display-2 mt-2">Cayce House</h1>
+        <h2>Web & App Developer</h2>
+        <div className="mt-5">
+          <SocialIcons />
+        </div>
+      </div>
+      <Img
+        fluid={data.bgImg.childImageSharp.fluid}
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: '100%',
+          height: '100%',
+          zIndex: -1,
+        }}
+      />
+      <GlobalStyle />
+    </React.Fragment>
+  </Layout>
+);
 
 Index.propTypes = {
   data: PropTypes.shape({ profileImg: PropTypes.object }).isRequired,
 };
+
+export default Index;
 
 export const query = graphql`
   query {
