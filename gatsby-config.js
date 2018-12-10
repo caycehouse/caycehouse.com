6b3d756e5c2096de6a648fa83f85c2d2c8dec1a4
Cayce House <cayce@houseco.cc>
2018-12-10 18:48:49 -0500

    Use system font instead of roboto

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,19 @@ module.exports = {
             type: 'dark',
           },
           typography: {
+            // Use the system font instead of the default Roboto font.
+            fontFamily: [
+              '-apple-system',
+              'BlinkMacSystemFont',
+              '"Segoe UI"',
+              'Roboto',
+              '"Helvetica Neue"',
+              'Arial',
+              'sans-serif',
+              '"Apple Color Emoji"',
+              '"Segoe UI Emoji"',
+              '"Segoe UI Symbol"',
+            ].join(','),
             useNextVariants: true,
           },
         },
