61e931a47faf09e9368f25b39ce9fc792d9bc6dc
Cayce House <cayce@houseco.cc>
2018-12-10 20:33:58 -0500

    Fixed Post Links

--- src/components/PostLink.jsx
+++ src/components/PostLink.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'gatsby';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
 
-const PostLink = ({ post }) => (
-  <div className="py-5">
-    <Link to={post.fields.slug}>
-      <h2>{post.fields.title}</h2>
-    </Link>
-    <p>{post.excerpt}</p>
-    <span className="text-muted">{post.fields.date}</span>
-  </div>
-);
+const styles = theme => ({
+  post: {
+    backgroundColor: theme.palette.background.paper,
+  },
+});
+
+const PostLink = props => {
+  const { classes, post } = props;
+  return (
+    <Card className={classes.post}>
+      <CardActionArea component={Link} to={post.fields.slug}>
+        <CardHeader title={post.fields.title} subheader={post.fields.date} />
+        <CardContent>
+          <Typography component="p">{post.excerpt}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button
+          size="small"
+          color="primary"
+          component={Link}
+          to={post.fields.slug}
+        >
+          Read More
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
 
 PostLink.propTypes = {
+  classes: PropTypes.shape({
+    post: PropTypes.string.isRequired,
+  }).isRequired,
   post: PropTypes.shape({
-    excerpt: PropTypes.string,
-    fields: PropTypes.object,
+    excerpt: PropTypes.string.isRequired,
+    fields: PropTypes.object.isRequired,
   }).isRequired,
 };
 
-export default PostLink;
+export default withStyles(styles)(PostLink);
