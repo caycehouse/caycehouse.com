77959fdb881fa08b6aff40078d62076fee604d35
Cayce House <cayce@mobilemedics.com>
2020-04-09 00:03:55 -0400

    Laying out links

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Feature from "../components/feature"
 import SEO from "../components/seo"
+import Nav from "react-bootstrap/Nav"
 
 const IndexPage = () => (
   <Layout>
@@ -10,6 +11,14 @@ const IndexPage = () => (
     <Feature />
     <h1 className="display-1">Cayce House</h1>
     <h2 className="display-5">Web &amp; Mobile App Developer</h2>
+    <Nav className="justify-content-center mt-5" activeKey="/home">
+      <Nav.Item>
+        <Nav.Link href="/">Home</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/contact">Contact</Nav.Link>
+      </Nav.Item>
+    </Nav>
   </Layout>
 )
 
