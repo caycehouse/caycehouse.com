506d2236fa578837243c324cfc63a9f67d020d72
Cayce House <cayce@caycehouse.com>
2025-01-09 12:41:49 -0500

    feat: astro rewrite

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react()],
+  site: 'https://caycehouse.com',
+  vite: {
+    plugins: [nodePolyfills()]
+  }
+});
---
+++ src/components/SocialNav.tsx
@@ -0,0 +1,24 @@
+import { FaEnvelope, FaGithub } from "react-icons/fa"
+import { IconContext } from "react-icons"
+import Nav from "react-bootstrap/Nav"
+
+export default function SocialNav() {
+  return (
+    <>
+      <Nav className="justify-content-center mt-5" activeKey="/home">
+        <IconContext.Provider value={{ size: "4em" }}>
+          <Nav.Item>
+            <Nav.Link href="https://github.com/caycehouse" target="_blank">
+              <FaGithub />
+            </Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="mailto:cayce@caycehouse.com" target="_blank">
+              <FaEnvelope />
+            </Nav.Link>
+          </Nav.Item>
+        </IconContext.Provider>
+      </Nav>
+    </>
+  )
+}
