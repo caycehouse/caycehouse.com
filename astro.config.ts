56dcf1140b6538fc4ece12c1433a33cf9eb6eeda
Cayce House <cayce@caycehouse.com>
2025-01-10 00:17:08 -0500

    feat: replace bootstrap with tailwind (#1305)
    * feat: replace bootstrap with tailwind
    
    * feat: finish replacing bootstrap

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
-
 import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
   site: 'https://caycehouse.com',
   vite: {
     plugins: [nodePolyfills() as any]
