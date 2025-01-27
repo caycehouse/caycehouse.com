f802f5e6b85dec29ced53e071049bba93b43e6ce
Cayce House <cayce@caycehouse.com>
2025-01-27 16:08:21 -0500

    feat: tailwind v4

--- astro.config.ts
+++ astro.config.ts
@@ -2,13 +2,16 @@
 import { defineConfig } from 'astro/config';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
   site: 'https://caycehouse.com',
   vite: {
-    plugins: [nodePolyfills() as any]
+    plugins: [,
+      nodePolyfills() as any,
+      tailwindcss()
+    ]
   }
 });
