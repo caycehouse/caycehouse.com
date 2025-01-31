9ea4f540c60ec044bf7d9dba4b39cf254a273276
Cayce House <cayce@caycehouse.com>
2025-01-30 22:16:56 -0500

    feat: replace react-icons with astro-icon

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import { nodePolyfills } from 'vite-plugin-node-polyfills';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), icon()],
   site: 'https://caycehouse.com',
   vite: {
     plugins: [,
